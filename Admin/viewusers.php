8472b2357e15fcb98d5e6c87e69d6c9174595d8d
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2023-12-25 14:36:56 +0600

    view page

--- Admin/viewusers.php
+++ Admin/viewusers.php
@@ -63,29 +63,64 @@ foreach ($users as $user) {
 }
 
 $content .= "
-<div class='card pd-20 pd-sm-40'>
-    <h6 class='card-body-title'>View Users</h6>
-    <div class='table-wrapper'>
-        <table id='datatable1' class='table display responsive nowrap'>
-            <thead>
-                <tr>
-                    <th class='wd-15p'>ID</th>
-                    <th class='wd-15p'>Name</th>
-                    <th class='wd-20p'>E-mail</th>
-                    <th class='wd-15p'>Role</th>
-                    <th class='wd-10p'>Verified</th>
-                </tr>
-            </thead>
-            <tbody>
-                $tableRows
-            </tbody>
-        </table>
-    </div><!-- table-wrapper -->
-</div><!-- card -->
+<div class='row row-sm mg-t-20'>
+        <div class='col-xl-8 card pd-20 pd-sm-40'>
+            <h6 class='card-body-title'>View Users</h6>
+            <div class='table-wrapper'>
+                <table id='datatable1' class='table display responsive nowrap'>
+                    <thead>
+                        <tr>
+                            <th class='wd-15p'>ID</th>
+                            <th class='wd-15p'>Name</th>
+                            <th class='wd-20p'>E-mail</th>
+                            <th class='wd-15p'>Role</th>
+                            <th class='wd-10p'>Verified</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        $tableRows
+                    </tbody>
+                </table>
+            </div><!-- table-wrapper -->
+        </div><!-- card -->
+        <div class='col-xl-4 mg-t-20 mg-xl-t-0 bg-white'>
+            <div class='card pd-20 pd-sm-40 form-layout form-layout-4'>
+                <div class='signin-logo tx-center tx-24 tx-bold tx-inverse'>Hostel Management <span
+                        class='tx-info tx-normal'>admin</span></div>
+
+                <form id='createUserForm'>
+                    <div class='form-group'>
+                        <input type='text' class='form-control' name='username' placeholder='Enter username'>
+                    </div><!-- form-group -->
+                    <div class='form-group'>
+                        <input type='email' class='form-control' name='email' placeholder='Enter email address'>
+                    </div><!-- form-group -->
+                    <div class='form-group'>
+                        <input type='password' class='form-control' name='password'
+                            placeholder='Enter password / default is password'>
+                    </div><!-- form-group -->
+                    <div class='form-group'>
+                        <select class='form-control select2' name='role' data-placeholder='Choose role'>
+                            <option value='admin'> Admin</option>
+                            <option value='student'> Student</option>
+                            <option value='accounts'> Accounts</option>
+                            <option value='provost'> Provost</option>
+                            <option value='hallsuper'> Hall Super</option>
+                        </select>
+                    </div>
+
+                    <button type='submit' class='btn btn-info btn-block'>Create</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
 ";
 $script = "$(function(){
     'use strict';
-
+    $('.select2').select2({
+        minimumResultsForSearch: Infinity
+    });
     $('#datatable1').DataTable({
       responsive: true,
       language: {
@@ -104,6 +139,70 @@ $script = "$(function(){
     // Select2
     $('.dataTables_length select').select2({ minimumResultsForSearch: Infinity });
 
-  });";
+  function createUser() {
+    var formData = {
+        action:'createUser',
+        username: $('input[name=\"username\"]').val(),
+        email: $('input[name=\"email\"]').val(),
+        password: $('input[name=\"password\"]').val(),
+        role: $('select[name=\"role\"]').val()
+    };
+
+    if (!formData.username || !formData.email || !formData.password || !formData.role) {
+        showAlert('warning', 'Please fill in all required fields.');
+        return;
+    }
+
+    // Perform AJAX request to your PHP script for user creation
+    $.ajax({
+        type: 'POST',
+        url: 'process.php',
+        data: formData,
+        success: function(response) {
+            if (response.success) {
+                showAlert('success', response.success);
+                $('#createUserForm')[0].reset();
+            } else if (response.error) {
+                showAlert('error', response.error);
+            }
+        },
+        error: function(error) {
+            console.error(error);
+            showAlert('error', 'An error occurred while processing your request.');
+        }
+    });
+    console.log(formData);
+}
+
+function showAlert(type, message) {
+    var alertClasses = {
+      'success': 'alert-success',
+      'danger': 'alert-danger',
+      'info': 'alert-info',
+      'warning': 'alert-warning',
+      'primary': 'alert-primary',
+      'secondary': 'alert-secondary'
+    };
+
+    var alertClass = alertClasses[type] || 'alert-info';
+    var alertHtml = '<div class=\"alert ' + alertClass + ' alert-dismissible fade show\" role=\"alert\">' +
+        message +
+        '<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">' +
+        '<span aria-hidden=\"true\">&times;</span>' +
+        '</button>' +
+        '</div>';
+
+    // Append the alert to a container (e.g., a div with id=\"alerts\")
+    $('#alerts').html(alertHtml);
+    }
+
+    // Add event listener for form submission
+    $('#createUserForm').on('submit', function(e) {
+        e.preventDefault();
+        createUser();
+    });
+
+});
+";
 
 include '../templates/base.php';
